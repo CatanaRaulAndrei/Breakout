f87c43a93b5631404179be14a355016dd249795d
CatanaRaulAndrei <raul19@windowslive.com>
2020-09-28 21:38:42 +0300

    breakout

---
+++ BreakoutGame/canvas.js
@@ -0,0 +1,171 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let score = 0;
+const brickColumn = 5;
+const brickRow = 9;
+
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+const bricks = [];
+for (let i = 0; i < brickRow; i++) {
+    bricks[i] = [];
+    for (let j = 0; j < brickColumn; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
+}
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 9,
+    speed: 3,
+    dx: 4,
+    dy: -4
+}
+const paddle = {
+    x: canvas.width / 2 - 50,
+    y: canvas.height - 20,
+    w: 90,
+    h: 10,
+    speed: 5,
+    dx: 0
+}
+
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#000000' : 'transparent';
+            ctx.fill();
+            ctx.closePath();
+        })
+    })
+}
+
+function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillText(`Scorul:${score}`, canvas.width - 110, 40);
+}
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = '#000000';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillStyle = '#000000';
+    ctx.fill();
+    ctx.closePath();
+}
+function movePaddle() {
+    paddle.x += paddle.dx;
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+    if (paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+        ball.dx *= -1;
+    }
+    if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+
+    if (ball.x - ball.size > paddle.x && ball.x + ball.size < paddle.x + paddle.w && ball.y + ball.size > paddle.y) {
+        ball.dy = -ball.speed;
+    }
+
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if (brick.visible) {
+                if (ball.x - ball.size > brick.x &&
+                    ball.x + ball.size < brick.x + brick.w &&
+                    ball.y + ball.size > brick.y &&
+                    ball.y - ball.size < brick.y + brick.h
+
+                ) {
+                    ball.dy *= -1;
+                    brick.visible = false;
+                    increaseScore();
+                }
+            }
+        })
+    })
+    if (ball.y + ball.size > canvas.height) {
+        showAllBricks();
+        score = 0;
+    }
+
+}
+
+function increaseScore() {
+    score++;
+    if (score % (brickRow * brickRow) === 0) {
+        showAllBricks();
+    }
+}
+function showAllBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => (brick.visible = true));
+    })
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawBricks();
+}
+
+
+function update() {
+    movePaddle();
+    moveBall();
+    draw();
+    requestAnimationFrame(update);
+}
+
+update();
+
+function keyDown(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        paddle.dx = paddle.speed;
+    }
+    else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+    }
+}
+
+function keyUp(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = 0;
+    }
+}
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+
